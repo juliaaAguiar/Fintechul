5b76f2e65badb32e41072b3f679e6923d0b37d30
pedroarthuralvesdeoliveira <pedroarthuralvesdeoliveira@hotmail.com>
2024-06-16 10:58:42 -0300

    Remoção de objetos Despesa e Deposito

--- src/main/java/br/com/fiap/fintech/bean/Extrato.java
+++ src/main/java/br/com/fiap/fintech/bean/Extrato.java
@@ -5,8 +5,6 @@ import java.util.List;
 public class Extrato {
   public List<Despesa> despesas;
   public List<Deposito> depositos;
-  public Despesa despesa;
-  public Deposito deposito;
 
   public Extrato() {
     super();
